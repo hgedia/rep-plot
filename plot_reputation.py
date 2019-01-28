0406b6da91f740bf60bf48ba06e689449dac8af2
hgedia <hgedia@hgedia.co>
2019-01-28 15:35:25 +0000

    Distribution of scores

--- plot_reputation.py
+++ plot_reputation.py
@@ -16,10 +16,10 @@ class Validator():
     def compute_score(self):
         for idx,_ in enumerate(self.reputation_data):
             params = self.reputation_data[idx]["params"];
-            vote_score  =0;
-            upvote_score =0;
-            downvote_score = 0;
-            hide_score = 0;
+            vote_score  =0
+            upvote_score =0
+            downvote_score = 0
+            hide_score = 0
 
             vote_score = self.reputation_data[idx]["score_params"]["vote_score"];
 
@@ -43,8 +43,8 @@ class Validator():
             self.annotate.append(self.reputation_data[idx]["annotation"])
 
         self.line, = plotter.plot(self.x,self.y,marker="o")
-        self.line.set_label(self.user_id[:2] + "..." + self.user_id[-2:])
-        plotter.legend(loc='best')
+        #self.line.set_label(self.user_id[:2] + "..." + self.user_id[-2:])
+        #plotter.legend(loc='best')
 
     def get_line(self):
         return self.line
@@ -71,33 +71,13 @@ class Validator():
         t = params["totalVotes"]
         v = self.reputation_data[l-1]["score_params"]["vote_score"]
         return t,u,d,h,v
-        
-validatorList = []
-
-with open("indorse-prod.validatorsv6.json") as f:
-    data = json.loads(f.read())
-    for idx,validators in enumerate(data):
-        validator = Validator(data[idx])
-        validatorList.append(validator)
- 
-fig, ax = plt.subplots()
-ax.set_xlabel("Data point")
-ax.set_ylabel("Validator Reputation")
-
-annot = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="w"),
-                    arrowprops=dict(arrowstyle="->"))
-annot.set_visible(False)
 
 
-#validatorList.sort(reverse=True)
-for validator in validatorList:
-    validator.plot_validator(plt)
-
-
-
-
-    
+    def get_last_reputation_score(self):
+        l = len(self.reputation_data)
+        s = self.reputation_data[l-1]["score"]
+        return s
+#Helpers    
 def plot_validator_metrics(validatorList,plotter):
     total_vote_count = 0
     total_votes_dict = {"0-10": 0, "10-20": 0, "20-30": 0, "30-40": 0, "40-INF": 0}
@@ -113,66 +93,71 @@ def plot_validator_metrics(validatorList,plotter):
 
     for validator in validatorList:
         t, u, d, h, v = validator.get_last_reputation_params()
-        print("T="+str(t) + " U="+str(u) + " D="+str(d) + " H="+str(h) +" V="+str(v));
+        #print("T="+str(t) + " U="+str(u) + " D="+str(d) + " H="+str(h) +" V="+str(v));
 
         #Total Votes
-        total_vote_count += t
-        if(t < 10):
-            total_votes_dict["0-10"] += 1
-        elif(t < 20):
-            total_votes_dict["10-20"] += 1
-        elif(t < 30):
-            total_votes_dict["30-40"] += 1
-        elif(t < 40):
-            total_votes_dict["30-40"] += 1
-        elif(t > 40):
-            total_votes_dict["40-INF"] += 1
+        if(t>0):
+            total_vote_count += 1
+            if(t < 10):
+                total_votes_dict["0-10"] += 1
+            elif(t < 20):
+                total_votes_dict["10-20"] += 1
+            elif(t < 30):
+                total_votes_dict["30-40"] += 1
+            elif(t < 40):
+                total_votes_dict["30-40"] += 1
+            elif(t >= 40):
+                total_votes_dict["40-INF"] += 1
 
         #Total Upvotes
-        total_upvote_count += u
-        if(u < 2):
-            total_upvotes_dict["0-2"] += 1
-        elif(u < 4):
-            total_upvotes_dict["2-4"] += 1
-        elif(u < 8):
-            total_upvotes_dict["4-8"] += 1
-        elif(u > 8):
-            total_upvotes_dict["8-INF"] += 1
+        if(u>0):
+            total_upvote_count += 1
+            if(u < 2):
+                total_upvotes_dict["0-2"] += 1
+            elif(u < 4):
+                total_upvotes_dict["2-4"] += 1
+            elif(u < 8):
+                total_upvotes_dict["4-8"] += 1
+            elif(u >= 8):
+                total_upvotes_dict["8-INF"] += 1
 
         #Total Downvotes
-        total_downvote_count += d
-        if(d < 2):
-            total_downvotes_dict["0-2"] += 1
-        elif(d < 4):
-            total_downvotes_dict["2-4"] += 1
-        elif(d < 8):
-            total_downvotes_dict["4-8"] += 1
-        elif(d > 8):
-            total_downvotes_dict["8-INF"] += 1
+        if(d >0):
+            total_downvote_count += 1
+            if(d < 2):
+                total_downvotes_dict["0-2"] += 1
+            elif(d < 4):
+                total_downvotes_dict["2-4"] += 1
+            elif(d < 8):
+                total_downvotes_dict["4-8"] += 1
+            elif(d >= 8):
+                total_downvotes_dict["8-INF"] += 1
 
         #Total Hides
-        total_hide_count += h
-        if(h < 2):
-            total_hide_dict["0-2"] += 1
-        elif(h < 4):
-            total_hide_dict["2-4"] += 1
-        elif(h < 8):
-            total_hide_dict["4-8"] += 1
-        elif(h > 8):
-            total_hide_dict["8-INF"] += 1
+        if(h > 0):
+            total_hide_count +=1
+            if(h < 2):            
+                total_hide_dict["0-2"] += 1
+            elif(h < 4):
+                total_hide_dict["2-4"] += 1
+            elif(h < 8):
+                total_hide_dict["4-8"] += 1
+            elif(h >= 8):
+                total_hide_dict["8-INF"] += 1
 
         #Total Consensus percentage
-        total_consensus_perc += v
-        if(v < 20):
-            total_consesnsus_perc_dict["0-20"] += 1
-        elif(v < 40):
-            total_consesnsus_perc_dict["20-40"] += 1
-        elif(v < 60):
-            total_consesnsus_perc_dict["40-60"] += 1
-        elif(v<80):
-            total_consesnsus_perc_dict["60-80"] += 1
-        elif(v > 80):
-            total_consesnsus_perc_dict["80-100"] += 1
+        if(v > 0):
+            total_consensus_perc += 1
+            if(v < 20):
+                total_consesnsus_perc_dict["0-20"] += 1
+            elif(v < 40):
+                total_consesnsus_perc_dict["20-40"] += 1
+            elif(v < 60):
+                total_consesnsus_perc_dict["40-60"] += 1
+            elif(v<80):
+                total_consesnsus_perc_dict["60-80"] += 1
+            elif(v >= 80):
+                total_consesnsus_perc_dict["80-100"] += 1
 
     #print(total_consesnsus_perc_dict)
 
@@ -186,19 +171,19 @@ def plot_validator_metrics(validatorList,plotter):
 
     plt.subplot(231)
     plotter.bar(total_consesnsus_perc_dict.keys(),total_consesnsus_perc_dict.values())
-    ax1.set_title("Consensus %")
+    ax1.set_title("Consensus % (T=" + str(total_consensus_perc) + ")")
     plt.subplot(232)
     plotter.bar(total_hide_dict.keys(),total_hide_dict.values())
     ax2.set_title("Hidden Votes (T=" + str(total_hide_count) +")")
     plt.subplot(233)
     plotter.bar(total_downvotes_dict.keys(), total_downvotes_dict.values())
-    ax3.set_title("Downvotes")
+    ax3.set_title("Downvotes (T=" + str(total_downvote_count) + ")")
     plt.subplot(234)
     plotter.bar(total_upvotes_dict.keys(), total_upvotes_dict.values())
-    ax4.set_title("Upvotes")
+    ax4.set_title("Upvotes (T=" + str(total_upvote_count) + ")")
     plt.subplot(235)
     plotter.bar(total_votes_dict.keys(), total_votes_dict.values())
-    ax5.set_title("Total Votes cast")
+    ax5.set_title("Votes cast by validator  (T=" + str(total_vote_count) + ")")
 
 
 def search_and_generate_annotation(event):
@@ -229,11 +214,66 @@ def hover(event):
                 fig.canvas.draw_idle()
 
 
+def plot_validator_score_bar(validatorList,plotter):
+    fig3 = plotter.figure()
+    validator_score_dict = {"0-10":0 , "10-20" :0 , "20-30":0, "30-40":0, "40-50" :0 , "50-60":0 , "60-70":0,"70-80":0,"80-90":0,"90-100":0}
+    total_validator_count = 0
+    for validator in validatorList:
+        s = validator.get_last_reputation_score()
+        if(s>0):
+            total_validator_count+=1
+            if(s <10):
+                validator_score_dict["0-10"] += 1
+            elif(s<20):
+                validator_score_dict["10-20"] += 1
+            elif(s < 30):
+                validator_score_dict["20-30"] += 1
+            elif(s < 40):
+                validator_score_dict["30-40"] += 1
+            elif(s < 50):
+                validator_score_dict["40-50"] += 1
+            elif(s < 60):
+                validator_score_dict["50-60"] += 1
+            elif(s<70):
+                validator_score_dict["60-70"] +=1
+            elif(s<80):
+                validator_score_dict["70-80"] += 1
+            elif(s < 90):
+                validator_score_dict["80-90"] += 1
+            elif(s>=90):
+                validator_score_dict["90-100"] += 1
+
+    #ax1 = fig3.add_subplot(351)
+    fig3.suptitle("Validator scores (T=" + str(total_validator_count) + ")")
+    plotter.bar(validator_score_dict.keys(),validator_score_dict.values())
+    
+
+
+#Main
+validatorList = []
+
+with open("indorse-prod.validatorsv6.json") as f:
+    data = json.loads(f.read())
+    for idx, validators in enumerate(data):
+        validator = Validator(data[idx])
+        validatorList.append(validator)
+
+fig, ax = plt.subplots()
+ax.set_xlabel("Data point")
+ax.set_ylabel("Validator Reputation")
+
+annot = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+annot.set_visible(False)
+
 fig.canvas.mpl_connect("motion_notify_event", hover)
 
+#Plot 
+for validator in validatorList:
+    validator.plot_validator(plt)
+
 plot_validator_metrics(validatorList,plt)
-
-
-
+plot_validator_score_bar(validatorList,plt)
 
 plt.show()
